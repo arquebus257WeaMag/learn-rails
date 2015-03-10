94fd656ea7feebed83802040ae526ba5d904aae6
Brad Cantrell <brad777mx@hotmail.com>
2015-03-09 20:34:18 -0700

    finished validation of submit form with mvc

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,6 +9,6 @@ class Contact
   validates_presence_of :content 
   validates_format_of :email, 
     :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i 
-  validates_length_of :content, :maximum = > 500 
+  validates_length_of :content, :maximum => 500 
 
 end
